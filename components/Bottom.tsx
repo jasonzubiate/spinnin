90e09254fbb98d7ce1836519eebeb2bb2d240352
Jason Zubiate <jzubiate.dev@gmail.com>
2024-02-05 01:21:03 -0800

    feature: adds GeistMono font

--- components/Bottom.tsx
+++ components/Bottom.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { GeistMono } from "geist/font/mono";
 import { useEffect, useState } from "react";
 import { getLocalTimeLosAngeles } from "@/utils";
 
@@ -21,12 +22,14 @@ export default function Bottom() {
   }, []);
 
   return (
-    <div className="flex w-full justify-end p-4 fixed bottom-0 md:justify-between">
-      <p className="geist uppercase hidden md:flex">Los Angeles ({time})</p>
+    <div
+      className={`${GeistMono.className} flex w-full justify-end p-4 fixed bottom-0 md:justify-between`}
+    >
+      <p className="uppercase hidden md:flex">Los Angeles ({time})</p>
       <a
         href="https://open.spotify.com/playlist/0NWr5vXsKMh7LEZeGNl7gB?si=db10c24b8ed1477e"
         target="_blank"
-        className="geist uppercase text-xs md:text-base"
+        className="uppercase text-xs md:text-base"
       >
         Tunes
       </a>
--- components/ContactModal.tsx
+++ components/ContactModal.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { GeistMono } from 'geist/font/mono'
 import submitContactForm from "@/actions/submitContactForm";
 import { emailRedirect } from "@/utils";
 import { Dispatch, SetStateAction, useEffect, useRef } from "react";
@@ -32,12 +33,12 @@ export default function ContactModal({ toggleModal }: ContactModalProps) {
   }, [toggleModal]);
 
   return (
-    <Draggable handle="#contact-modal-header">
+    <Draggable handle="#contact-modal-header" >
       <form
         action={submitContactForm}
         ref={modalRef}
         id="contact-modal"
-        className="w-4/5 lg:w-[650px] flex flex-col absolute z-20 "
+        className={`${GeistMono.className} w-4/5 lg:w-[650px] flex flex-col absolute z-20`}
       >
         <div
           id="contact-modal-header"
@@ -56,12 +57,12 @@ export default function ContactModal({ toggleModal }: ContactModalProps) {
         </div>
         <div className="h-[400px] flex flex-col p-4 gap-3 bg-[#1E1E1E] text-demo-smoke">
           <div className="flex flex-col">
-            <p className="geist text-sm font-light">Spinnin.studio</p>
-            <p className="geist text-sm font-light">
+            <p className="text-sm font-light">Spinnin.studio</p>
+            <p className="text-sm font-light">
               ©{new Date().getFullYear()}
             </p>
           </div>
-          <p className="geist text-sm font-light">
+          <p className="text-sm font-light">
             Links:{" "}
             <a
               href="https://instagram.com/spinnin.studio"
@@ -87,24 +88,24 @@ export default function ContactModal({ toggleModal }: ContactModalProps) {
               Founder
             </a>
           </p>
-          <p className="geist text-sm font-light">
+          <p className="text-sm font-light">
             For all inquiries, please contact{" "}
             <a className="cursor-pointer text-blue-400" onClick={emailRedirect}>
               hey.spinnin@gmail.com
             </a>
             .
           </p>
-          <p className="geist text-sm font-light">
+          <p className="text-sm font-light">
             How about we do a thing or two. For project requests, drop us a
             message below. We&apos;re looking forward to connecting with you.
           </p>
-          <p className="geist text-sm font-light">Your message:</p>
+          <p className="text-sm font-light">Your message:</p>
           <div className="flex gap-2 items-center">
             <div className="w-[6px] h-[6px] bg-[#7FF039] pulse rounded-full"></div>
             <input
               name="message"
               type="text"
-              className="bg-inherit geist text-sm font-light focus:outline-none w-full"
+              className="bg-inherit text-sm font-light focus:outline-none w-full"
               autoFocus
               onKeyDown={(e) => {
                 if (e.key === "Enter") {
