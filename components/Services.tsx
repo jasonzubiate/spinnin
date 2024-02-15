85e222a18bab05ba7abd7f85c968a9d4a3f38e14
Jason Zubiate <jzubiate.dev@gmail.com>
2024-02-14 18:05:10 -0800

    refactor: style changes

--- components/Services.tsx
+++ components/Services.tsx
@@ -5,7 +5,7 @@ import img2 from "@/public/img/developmentPhoto.png";
 
 export default function Services() {
   return (
-    <section id="services" className="pt-24 flex flex-col gap-24">
+    <section id="services" className="pt-24 flex flex-col gap-16 md:gap-24">
       <div className="flex flex-col px-4 md:px-6 gap-4 md:gap-8">
         <h3 className={`${GeistMono.className} text-sm uppercase`}>Services</h3>
         <p className="text-[3vw] md:text-[2vw] leading-tight uppercase md:w-3/5">
@@ -25,46 +25,71 @@ export default function Services() {
             />
           </div>
           <div className="px-4 md:px-6 pt-4 pb-16 flex flex-col gap-4 lg:border-r-[.5px] border-[#7f7f7f]">
-            <label htmlFor="service-design" className="text-3xl uppercase">
+            <label
+              htmlFor="service-design"
+              className="text-2xl md:text-3xl uppercase"
+            >
               Design
             </label>
             <div className="flex gap-12">
               <div className="flex flex-col">
-                <p className={`${GeistMono.className} text-[#7f7f7f]`}>
+                <p
+                  className={`${GeistMono.className} text-sm md:text-base text-[#7f7f7f]`}
+                >
                   UI UX Design
                 </p>
-                <p className={`${GeistMono.className} text-[#7f7f7f]`}>
+                <p
+                  className={`${GeistMono.className} text-sm md:text-base text-[#7f7f7f]`}
+                >
                   Moible App Design
                 </p>
-                <p className={`${GeistMono.className} text-[#7f7f7f]`}>
+                <p
+                  className={`${GeistMono.className} text-sm md:text-base text-[#7f7f7f]`}
+                >
                   Research & Discovery
                 </p>
-                <p className={`${GeistMono.className} text-[#7f7f7f]`}>
+                <p
+                  className={`${GeistMono.className} text-sm md:text-base text-[#7f7f7f]`}
+                >
                   Competitive Analysis
                 </p>
               </div>
               <div className="flex flex-col">
-                <p className={`${GeistMono.className} text-[#7f7f7f]`}>
+                <p
+                  className={`${GeistMono.className} text-sm md:text-base text-[#7f7f7f]`}
+                >
                   Website Design
                 </p>
-                <p className={`${GeistMono.className} text-[#7f7f7f]`}>
+                <p
+                  className={`${GeistMono.className} text-sm md:text-base text-[#7f7f7f]`}
+                >
                   Motion Design
                 </p>
-                <p className={`${GeistMono.className} text-[#7f7f7f]`}>
+                <p
+                  className={`${GeistMono.className} text-sm md:text-base text-[#7f7f7f]`}
+                >
                   Interaction Design
                 </p>
-                <p className={`${GeistMono.className} text-[#7f7f7f]`}>
+                <p
+                  className={`${GeistMono.className} text-sm md:text-base text-[#7f7f7f]`}
+                >
                   Iconography
                 </p>
               </div>
               <div className="flex flex-col">
-                <p className={`${GeistMono.className} text-[#7f7f7f]`}>
+                <p
+                  className={`${GeistMono.className} text-sm md:text-base text-[#7f7f7f]`}
+                >
                   Typography
                 </p>
-                <p className={`${GeistMono.className} text-[#7f7f7f]`}>
+                <p
+                  className={`${GeistMono.className} text-sm md:text-base text-[#7f7f7f]`}
+                >
                   Design Systems
                 </p>
-                <p className={`${GeistMono.className} text-[#7f7f7f]`}>
+                <p
+                  className={`${GeistMono.className} text-sm md:text-base text-[#7f7f7f]`}
+                >
                   Prototyping
                 </p>
               </div>
@@ -81,35 +106,54 @@ export default function Services() {
             />
           </div>
           <div className="px-4 md:px-6 pt-4 pb-16 flex flex-col gap-4">
-            <label htmlFor="service-design" className="text-3xl uppercase">
+            <label
+              htmlFor="service-design"
+              className="text-2xl md:text-3xl uppercase"
+            >
               Development
             </label>
             <div className="flex gap-12">
               <div className="flex flex-col">
-                <p className={`${GeistMono.className} text-[#7f7f7f]`}>
+                <p
+                  className={`${GeistMono.className} text-sm md:text-base text-[#7f7f7f]`}
+                >
                   React Development
                 </p>
-                <p className={`${GeistMono.className} text-[#7f7f7f]`}>
+                <p
+                  className={`${GeistMono.className} text-sm md:text-base text-[#7f7f7f]`}
+                >
                   Next.js Development
                 </p>
-                <p className={`${GeistMono.className} text-[#7f7f7f]`}>
+                <p
+                  className={`${GeistMono.className} text-sm md:text-base text-[#7f7f7f]`}
+                >
                   Front-end Development
                 </p>
-                <p className={`${GeistMono.className} text-[#7f7f7f]`}>
+                <p
+                  className={`${GeistMono.className} text-sm md:text-base text-[#7f7f7f]`}
+                >
                   CMS Development
                 </p>
               </div>
               <div className="flex flex-col">
-                <p className={`${GeistMono.className} text-[#7f7f7f]`}>
+                <p
+                  className={`${GeistMono.className} text-sm md:text-base text-[#7f7f7f]`}
+                >
                   Website Development
                 </p>
-                <p className={`${GeistMono.className} text-[#7f7f7f]`}>
+                <p
+                  className={`${GeistMono.className} text-sm md:text-base text-[#7f7f7f]`}
+                >
                   Framer Website
                 </p>
-                <p className={`${GeistMono.className} text-[#7f7f7f]`}>
+                <p
+                  className={`${GeistMono.className} text-sm md:text-base text-[#7f7f7f]`}
+                >
                   Squarespace Website
                 </p>
-                <p className={`${GeistMono.className} text-[#7f7f7f]`}>
+                <p
+                  className={`${GeistMono.className} text-sm md:text-base text-[#7f7f7f]`}
+                >
                   GSAP Animations
                 </p>
               </div>
